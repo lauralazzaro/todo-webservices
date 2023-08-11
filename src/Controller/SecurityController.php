5d03d0d399bf612d3ddf585d83fa32b39aaf81f3
Laura Lazzaro <lz_laura@hotmail.com>
2023-08-11 23:12:12 +0200

    Test for SecurityController Class

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -30,6 +30,7 @@ class SecurityController extends AbstractController
     #[Route(path: '/logout', name: 'logout')]
     public function logout(): void
     {
+        // This condition is not coverable since it depends on external factors.
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
