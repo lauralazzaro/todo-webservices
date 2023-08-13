9be5b410f6b01b481cd1fdade54c3abbe2959cc3
Laura Lazzaro <lz_laura@hotmail.com>
2023-08-13 11:06:57 +0200

    Test for Default Controller

---
+++ tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndexRoute()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertSelectorTextContains('h1', 'Todo List');
+    }
+}
