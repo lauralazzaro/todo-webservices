e4b40dd56df054f17b56b26b8012fd08daa977ba
Laura Lazzaro <lz_laura@hotmail.com>
2024-04-29 15:29:15 +0200

    generate new migration file

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,7 +3,6 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
-use DateTimeImmutable;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
