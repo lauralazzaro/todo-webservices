dc859d0c01a08924487e7e080b10d50215ca3aa3
Laura Lazzaro <lz_laura@hotmail.com>
2024-06-13 09:54:41 +0200

    added webpack and datepicker

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
