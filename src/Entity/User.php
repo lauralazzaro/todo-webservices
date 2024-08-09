09e06ea55760b2cf1d1473ff8e0ed65a1db86103
Laura Lazzaro <lz_laura@hotmail.com>
2024-08-09 10:45:46 -0400

    json login

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -84,7 +84,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getUserIdentifier(): string
     {
-        return $this->email;
+        return $this->username;
     }
 
     /**
